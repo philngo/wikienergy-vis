4d047ef7741caef9e018383e6bb3251cea8c8967
Phil Ngo <ngo.phil@gmail.com>
2014-06-24 17:11:39 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WikienergyVis::Application.config.secret_key_base = '6f08eb78f8947ec6b86d5ba83b0b7dd33a1254aaf2d66e4f264c171deae438c3fbcf649072c33fd9887af5c24bf0e1bae767a5039d50fa83da47cb8f561c4825'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WikienergyVis::Application.config.session_store :cookie_store, key: '_wikienergy_vis_session'
