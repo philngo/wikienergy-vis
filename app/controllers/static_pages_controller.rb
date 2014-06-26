c6338e71e1e22c4175aa770d01656c22e1c39955
Phil Ngo <ngo.phil@gmail.com>
2014-06-26 09:31:41 -0500

    updated home data, partner map

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,13 @@
 class StaticPagesController < ApplicationController
 
   def appliances
+    houses = [5949,2158,5785,9737,7989,2156,9654,2829,6941,5746]
+    index = params["i"]
+    if index
+      @house = houses[index.to_i]
+    else
+      @house = houses[4]
+    end
   end
 
   def home
